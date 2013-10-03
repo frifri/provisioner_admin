044c115f73ae22da38732c2929e2a9064a280bc0
frifri <francis.genet@gmail.com>
2013-10-03 15:16:55 -0700

    Adding Bootstrap

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Provisioner admin</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body style="padding-top:50px;">
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Provisioner</a>
+                <ul class="nav navbar-nav">
+                    <li><a href="#">White labelling</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        BAISMWAAAAAAAAA
+    </div>
+</body>
+</html>
